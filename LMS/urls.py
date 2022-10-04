b0f7ed65f26eaf193440217dd682d8a0fef0bc64
cartel360 <billycartel360@gmail.com>
2022-10-04 12:49:46 +0300

    Dependency Fixes

--- LMS/urls.py
+++ LMS/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
+# from rest_framework.authtoken import views as authviews
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ckeditor_uploader.urls')),
-    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
+    # path('api-token-auth/', authviews.obtain_auth_token, name='api-token-auth'),
     path('api/v1/curriculum/', include('curriculum.urls')),
     path('api/v1/account/', include('account.urls')),
     path('api/v1/student/', include('student.urls')),
